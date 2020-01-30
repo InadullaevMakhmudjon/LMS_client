cd04b78431c96f22543b2efcca938ec2305886f2
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-01-30 20:39:24 +0500

    Authors service created

--- src/services/Authors.js
+++ src/services/Authors.js
@@ -2,5 +2,7 @@ import { execute, API } from './Api';
 
 export default {
     getAll: () => execute(API.get('authors')),
-    get: id => execute(API.get(`authors/${id}`))
+    get: id => execute(API.get(`authors/${id}`)),
+    create: author => execute(API.post('authors', { author })),
+    delete: id => execute(API.delete(`authors/${id}`)),
 }
