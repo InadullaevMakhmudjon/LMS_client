ac467def178d62260607ff19958fa358e9c6d0ee
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-01-30 20:39:33 +0500

    Categories service created

--- src/services/Categories.js
+++ src/services/Categories.js
@@ -2,5 +2,7 @@ import { execute, API } from './Api';
 
 export default {
     getAll: () => execute(API.get('categories', { attributes: ['id', 'name'] })),
-    get: id => execute(API.get(`categories/${id}`))
+    get: id => execute(API.get(`categories/${id}`)),
+    create: category => execute(API.post('categories', { category })),
+    delete: id => execute(API.delete(`categories/${id}`))
 }
